a208cdee4c19e384edd226a6a29912bbe69089c4
Mykola <kolanuch13@gmail.com>
2022-11-22 21:47:14 +0200

    First file was created.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let disabled = startBtn.getAttribute("disabled");
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000);
+    startBtn.setAttribute("disabled", "disabled");
+    // console.log("start");
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+    // console.log("stop");
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const inputDate = document.querySelector('#datetime-picker');
+    
+console.log(inputDate);
